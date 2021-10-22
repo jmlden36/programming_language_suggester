0b373bdf85b25e9053ac8574dee1f226782af747
Matt Luker <jamesmattluker@gmail.com>
2021-10-22 13:21:24 -0600

    add images and add python and javascript to showLanguage function

--- js/scripts.js
+++ js/scripts.js
@@ -5,14 +5,23 @@ function add(val1, val2, val3, val4, val5) {
 
 function showLanguage(total) {
   if (total < 10) {
-    $("div.output").append("<h3>You might like C#</h3>");
+    $("div.output").append("<h3>You might like C#!</h3>");
     $("div.output").append("<img class='img' alt='C# logo' src='img/clogo.jpeg'>");
     $("div.output").append("<p>C# information here.</p>");
-    } else {
-    alert("other value")
+  } else if ( total >= 10 && total < 50) {
+    $("div.output").append("<h3>You might like Python!<h3>");
+    $("div.output").append("<img class='img' alt='Python logo' src='img/python.jpeg'>");
+    $("div.output").append("<p>Python info here.</p>");
+  } else if (total >=50 && total <150) {
+    $("div.output").append("<h3>You might like JavaScript!</h3>");
+    $("div.output").append("<img class='img' src='img/java.jpeg'>");
+    $("div.output").append("<p>JavaScript info here.</p>");
+  } else {
+    alert("You broke it!");
   }
-  ("button").hide
+  $("#button").hide
 }
+
 
 $(document).ready(function() {
   $("form#formOne").submit(function(event) {
