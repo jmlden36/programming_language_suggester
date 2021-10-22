ee36cd4a473224352ce1d92278d7d00eb11abc45
Matt Luker <jamesmattluker@gmail.com>
2021-10-22 12:38:04 -0600

    fix typos in radios and add show language function

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,13 @@ function add(val1, val2, val3, val4, val5) {
   const total = val1 + val2 + val3 + val4 + val5;
   return total;
 }
+
+function showLanguage(total) {
+  if (total < 10) {
+    $("div.output").append("<h3>You might like C#</h3>");
+    $("div.output").append("<img class='img' src='img/C#.jpeg'>");
+    $("div.output").append("<p>C# information here.</p>");
+  } else {
+    alert("other value")
+  }
+}
